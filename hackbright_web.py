49b730469b1de34af2f80d641f0070eedf59f40b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-23 17:59:49 -0800

    Saving work...

--- hackbright_web.py
+++ hackbright_web.py
@@ -18,14 +18,20 @@ def homepage():
 def get_student():
     """Show information about a student."""
 
-    github = request.args.get('github')
+    # FIXME: remove if condition to make this work
+    # how do we pass information from projects to this page?
+    # POST method
 
-    first, last, github = hackbright.get_student_by_github(github)
+    if :
+    # only request this section if you're coming from the form
+        github = request.args.get('github')
 
-    all_grades = hackbright.get_grades_by_github(github)
+        first, last, github = hackbright.get_student_by_github(github)
+
+        all_grades = hackbright.get_grades_by_github(github)
 
     html = render_template("student_info.html", first=first, last=last,
-                           github=github, all_grades=all_grades)
+                               github=github, all_grades=all_grades)
 
     return html
 
@@ -56,6 +62,39 @@ def update_student_database():
 
     return render_template("student_confirmation.html", first_name=first_name,
                            last_name=last_name, github=github)
+
+
+@app.route("/project/<title>")
+def show_project(title):
+    """Show project information."""
+
+    project_name, description, max_grade = hackbright.get_project_by_title(title)
+    # unpacking the tuple :)
+
+    gh_name_dict = {}
+
+    accounts_and_grades = hackbright.get_grades_by_title(title)
+
+    for student_account in accounts_and_grades:
+    # loop through accounts & grades list to parse students
+        gh_account = student_account[0]
+        grade = student_account[1]
+        first_name, last_name, github = hackbright.get_student_by_github(gh_account)
+        # unpacking elements from tuple
+        gh_name_dict[github] = (first_name + " " + last_name, grade,)
+        # assigning github UN as key and full name and grade as values
+
+    student_grade_pair = gh_name_dict.values()
+    # stripping off key to leave paired values (grade and full name)
+    # passing to Jinja in template
+
+    return render_template("project.html",
+                           max_grade=max_grade,
+                           project_name=project_name,
+                           description=description,
+                           students=student_grade_pair
+                           )
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
